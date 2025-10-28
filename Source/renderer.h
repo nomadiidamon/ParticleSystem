e968c7f4af7aea44b7b4ffe3e08324cd72bc5866
Damon S. Green II <damonsgreen2@gmail.com>
2025-10-28 07:58:58 -0400

    auto commit

--- Source/renderer.h
+++ Source/renderer.h
@@ -103,7 +103,7 @@ public:
 		InitializeDescriptorLayout();
 
 		// TODO: Part 3a
-		InitializeViewMatrix(0.0f, -0.25f, -0.25f, -5.0f, 0.0f);
+		InitializeViewMatrix(0.0f, 0.25f, -0.85f, -5.0f, 0.0f);
 		// TODO: Part 3c
 		float aspectRatio = 0.0f;
 		vlk.GetAspectRatio(aspectRatio);
