e611971b9661fc400407ef97bb960e65dfec3b37
Damon S. Green II <damonsgreen2@gmail.com>
2025-10-29 21:44:26 -0400

    auto commit

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,63 +3,178 @@
 #include "APP/App.h"
 #include "precompiled.h"
 
+#define APP_NAME L"ParticleSystem"
+
 
 // Forward declare Win32 proc
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+// Global Vulkan handles
+VkInstance g_Instance = VK_NULL_HANDLE;
+VkPhysicalDevice g_PhysicalDevice = VK_NULL_HANDLE;
+VkDevice g_Device = VK_NULL_HANDLE;
+uint32_t g_QueueFamily = (uint32_t)-1;
+VkQueue g_Queue = VK_NULL_HANDLE;
+VkDescriptorPool g_DescriptorPool = VK_NULL_HANDLE;
+VkRenderPass g_RenderPass = VK_NULL_HANDLE;
+HWND g_hWnd = NULL;
+
+// Simple Vulkan error checker
+static void check_vk_result(VkResult err)
+{
+    if (err == 0) return;
+    std::cerr << "[vulkan] Error: VkResult = " << err << std::endl;
+    if (err < 0) abort();
+}
+
+// Minimal Vulkan setup
+void SetupVulkan()
+{
+    // Create Vulkan instance
+    VkInstanceCreateInfo create_info = {};
+    create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+    VkApplicationInfo app_info = {};
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.pApplicationName = "ImGui Vulkan Example";
+    app_info.apiVersion = VK_API_VERSION_1_1;
+    create_info.pApplicationInfo = &app_info;
+
+    check_vk_result(vkCreateInstance(&create_info, nullptr, &g_Instance));
+
+    // Pick first physical device
+    uint32_t gpu_count = 0;
+    vkEnumeratePhysicalDevices(g_Instance, &gpu_count, nullptr);
+    VkPhysicalDevice gpus[8];
+    vkEnumeratePhysicalDevices(g_Instance, &gpu_count, gpus);
+    g_PhysicalDevice = gpus[0];
+
+    // Find graphics queue family
+    uint32_t count;
+    vkGetPhysicalDeviceQueueFamilyProperties(g_PhysicalDevice, &count, nullptr);
+    VkQueueFamilyProperties* queues = new VkQueueFamilyProperties[count];
+    vkGetPhysicalDeviceQueueFamilyProperties(g_PhysicalDevice, &count, queues);
+    for (uint32_t i = 0; i < count; i++)
+    {
+        if (queues[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        {
+            g_QueueFamily = i;
+            break;
+        }
+    }
+    delete[] queues;
+
+    // Create logical device and queue
+    float queue_priority = 1.0f;
+    VkDeviceQueueCreateInfo queue_info = {};
+    queue_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queue_info.queueFamilyIndex = g_QueueFamily;
+    queue_info.queueCount = 1;
+    queue_info.pQueuePriorities = &queue_priority;
+
+    VkDeviceCreateInfo device_info = {};
+    device_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    device_info.queueCreateInfoCount = 1;
+    device_info.pQueueCreateInfos = &queue_info;
+
+    check_vk_result(vkCreateDevice(g_PhysicalDevice, &device_info, nullptr, &g_Device));
+    vkGetDeviceQueue(g_Device, g_QueueFamily, 0, &g_Queue);
+
+    // Create descriptor pool
+    VkDescriptorPoolSize pool_sizes[] =
+    {
+        { VK_DESCRIPTOR_TYPE_SAMPLER, 1000 },
+        { VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1000 },
+        { VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, 1000 },
+        { VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1000 },
+        { VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, 1000 },
+        { VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 1000 },
+        { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1000 },
+        { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1000 },
+        { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1000 },
+        { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, 1000 },
+        { VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 1000 }
+    };
+
+    VkDescriptorPoolCreateInfo pool_info = {};
+    pool_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    pool_info.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
+    pool_info.maxSets = 1000 * IM_ARRAYSIZE(pool_sizes);
+    pool_info.poolSizeCount = (uint32_t)IM_ARRAYSIZE(pool_sizes);
+    pool_info.pPoolSizes = pool_sizes;
+    check_vk_result(vkCreateDescriptorPool(g_Device, &pool_info, nullptr, &g_DescriptorPool));
+
+    // Create a dummy render pass
+    VkAttachmentDescription attachment = {};
+    attachment.format = VK_FORMAT_B8G8R8A8_UNORM;
+    attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    VkAttachmentReference color_attachment = {};
+    color_attachment.attachment = 0;
+    color_attachment.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass = {};
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &color_attachment;
+
+    VkRenderPassCreateInfo render_pass_info = {};
+    render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    render_pass_info.attachmentCount = 1;
+    render_pass_info.pAttachments = &attachment;
+    render_pass_info.subpassCount = 1;
+    render_pass_info.pSubpasses = &subpass;
+
+    check_vk_result(vkCreateRenderPass(g_Device, &render_pass_info, nullptr, &g_RenderPass));
+}
+
 
 int main()
 {
     // 1. Create Win32 window
     WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L,
                       GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
-                      L"ImGui Vulkan Example", NULL };
+                      APP_NAME, NULL };
     RegisterClassEx(&wc);
-    HWND hwnd = CreateWindow(wc.lpszClassName, L"Dear ImGui Vulkan Example",
+    HWND hwnd = CreateWindow(wc.lpszClassName, APP_NAME,
         WS_OVERLAPPEDWINDOW, 100, 100, 1280, 720,
         NULL, NULL, wc.hInstance, NULL);
+    g_hWnd = hwnd;
 
-    // 2. Initialize Vulkan (instance, device, swapchain, etc.)
-    // Normally you'd create VkInstance, VkDevice, VkRenderPass, etc.
-    // For simplicity, refer to ImGui’s example_vulkan/main.cpp for full Vulkan setup.
-    // Here we’ll assume you have a VkInstance, VkDevice, VkRenderPass ready.
-    //VkInstance instance = ...;
-    //VkDevice device = ...;
-    //VkRenderPass render_pass = ...;
-    //VkQueue queue = ...;
-    //uint32_t queue_family = ...;
-    //VkDescriptorPool descriptor_pool = ...;
+    // 2. Setup Vulkan
+    SetupVulkan();
 
-    // 3. Setup Dear ImGui context
+    // 3. Setup Dear ImGui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     ImGui::StyleColorsDark();
 
-    // 4. Setup Platform/Renderer backends
+    // 4. Initialize ImGui Vulkan/Win32 backends
     ImGui_ImplWin32_Init(hwnd);
     ImGui_ImplVulkan_InitInfo init_info = {};
-    //init_info.Instance = instance;
-    //init_info.PhysicalDevice = /* your physical device */;
-    //init_info.Device = device;
-    //init_info.QueueFamily = queue_family;
-    //init_info.Queue = queue;
-    init_info.PipelineCache = VK_NULL_HANDLE;
-    //init_info.DescriptorPool = descriptor_pool;
+    init_info.Instance = g_Instance;
+    init_info.PhysicalDevice = g_PhysicalDevice;
+    init_info.Device = g_Device;
+    init_info.QueueFamily = g_QueueFamily;
+    init_info.Queue = g_Queue;
+    init_info.DescriptorPool = g_DescriptorPool;
     init_info.MinImageCount = 2;
     init_info.ImageCount = 2;
-    //init_info.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
-	init_info.PipelineInfoMain.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
-    init_info.CheckVkResultFn = nullptr;
+    init_info.PipelineInfoMain.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
+    init_info.CheckVkResultFn = check_vk_result;
     ImGui_ImplVulkan_Init(&init_info);
 
-    //// Upload Fonts
-    //{
-    //    VkCommandBuffer command_buffer = /* begin single-use command buffer */;
-    //    ImGui_ImplVulkan_CreateFontsTexture(command_buffer);
-    //    /* end command buffer and submit */;
-    //    ImGui_ImplVulkan_DestroyFontUploadObjects();
-    //}
+    //// Upload fonts
+    //ImGui_ImplVulkan_CreateFontsTexture(VK_NULL_HANDLE);
+    //ImGui_ImplVulkan_DestroyFontUploadObjects();
 
     ShowWindow(hwnd, SW_SHOWDEFAULT);
     UpdateWindow(hwnd);
@@ -79,29 +194,34 @@ int main()
         if (done)
             break;
 
-        // Start new frame
+        // Start the frame
         ImGui_ImplVulkan_NewFrame();
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
 
-        // Draw GUI
-        ImGui::Begin("Hello!");
-        ImGui::Text("This is Dear ImGui running on Vulkan!");
+        // Your ImGui content
+        ImGui::Begin("Hello Vulkan!");
+        ImGui::Text("This is Dear ImGui running on Vulkan + Win32!");
         ImGui::End();
 
         // Render
         ImGui::Render();
-        // Record Vulkan commands to render ImGui draw data
-        //ImGui_ImplVulkan_RenderDrawData(ImGui::GetDrawData(), /*your_command_buffer*/);
+        // Normally you'd record draw commands here and present to a swapchain
+        // (omitted for brevity)
     }
 
     // Cleanup
+    vkDestroyDescriptorPool(g_Device, g_DescriptorPool, nullptr);
+    vkDestroyRenderPass(g_Device, g_RenderPass, nullptr);
+    vkDestroyDevice(g_Device, nullptr);
+    vkDestroyInstance(g_Instance, nullptr);
+
     ImGui_ImplVulkan_Shutdown();
     ImGui_ImplWin32_Shutdown();
     ImGui::DestroyContext();
+
     DestroyWindow(hwnd);
     UnregisterClass(wc.lpszClassName, wc.hInstance);
-
     return 0;
 }
 
