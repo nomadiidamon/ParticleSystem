3c81b30942101a78c751a0735e706ffad5b2d61a
Damon S. Green II <damonsgreen2@gmail.com>
2025-10-29 20:50:23 -0400

    auto commit

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
-#include "App/App.h"
-
+#include <windows.h>
+#include "precompiled.h"
 
 
 int main()
 {
 	std::cout << "Hello, World!\n" << std::endl;
 
-	ImGui::Begin("Hello!");
-	ImGui::NewFrame();
-
-	// Create application window
-	while (true)
-	{
-		ImGui::ShowAboutWindow();
-		ImGui::NewFrame();
-	}
-	//App app;
 	return -1; // failed to create Gateware application
 }
