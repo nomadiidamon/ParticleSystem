b40ba711c67871a4394a50c5675b823b61bfd932
Damon S. Green II <damonsgreen2@gmail.com>
2025-10-29 20:57:10 -0400

    auto commit

--- Source/main.cpp
+++ Source/main.cpp
@@ -20,6 +20,8 @@ int main()
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
 	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+	io.DisplaySize = ImVec2(1280, 720);  // any positive window size
+
 
 	// Setup Dear ImGui style
 	ImGui::StyleColorsDark();
