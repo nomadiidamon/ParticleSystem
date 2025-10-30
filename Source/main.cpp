f2048467d1c5124a1622982ec547245ba2faf073
Damon S. Green II <damonsgreen2@gmail.com>
2025-10-29 20:54:52 -0400

    auto commit

--- Source/main.cpp
+++ Source/main.cpp
@@ -11,7 +11,10 @@ int main()
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
-
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
 
 	// start a frame
 	ImGui::NewFrame();
